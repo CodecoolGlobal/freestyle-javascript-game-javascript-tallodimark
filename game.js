cdc69236b78dde342e81907014a6b87c72957caf
Balint Meszaros <meszaros.balint65@gmail.com>
2022-05-23 15:04:23 +0200

    Add: skeleton part 1

--- game.js
+++ game.js
@@ -1,7 +1,27 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const difficulty = urlParams.get('difficulty');
+
 initGame();
 
 function initGame() {
+    this.drawBoard();
+    this.initKeyUp();
 
     // Your game can start here, but define separate functions, don't write everything in here :)
 
+}
+function drawBoard () {
+ console.log("draw")
+}
+function initKeyUp () {
+    document.addEventListener('keyup', (event) => {
+    if (event.key === 'ArrowUp') {console.log(event.key)
+    } else if (event.key === 'ArrowDown') {console.log(event.key)
+    } else if (event.key === 'ArrowLeft') {console.log(event.key)
+    } else if (event.key === 'ArrowRight') {console.log(event.key)
+    } else if (event.key === ' ') {console.log(event.key)
+    } else {console.log('invalid')
+    }
+    })
 }
