1d7339645de9c38b45939c5ca6842b138f4c1a20
Tallódi Márk <mark.tallodi@gmail.com>
2022-05-24 09:00:59 +0200

    add: prepare skeleton

--- game.js
+++ game.js
@@ -1,19 +1,34 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const difficulty = urlParams.get('difficulty');
+let monsterNumber = 0;
+let coinNumber = 0;
+let chestNumber = 0;
+let swordNumber = 0;
+let monstertHp = 0;
+let heroHp = 0;
+let difficultyTimer = 0;
+if (difficulty === 'Easy') {
+    monsterNumber = 5;
+} else if (difficulty === 'Medium') {
+    monsterNumber = 10;
+} else if (difficulty === 'Hard') {
+    monsterNumber = 20;
+}
 
 initGame();
 
 function initGame() {
     this.drawBoard();
+    this.populateBoard(monsterNumber);
+    this.placeHero();
     this.initKeyUp();
-
-    // Your game can start here, but define separate functions, don't write everything in here :)
-
 }
+
 function drawBoard () {
- console.log("draw")
+    console.log("draw");
 }
+
 function initKeyUp () {
     document.addEventListener('keyup', (event) => {
     if (event.key === 'ArrowUp') {console.log(event.key)
@@ -24,4 +39,16 @@ function initKeyUp () {
     } else {console.log('invalid')
     }
     })
+}
+
+function populateBoard(monsterNumber) {
+    console.log(monsterNumber);
+}
+
+function moveMonserts(monsterNumber, difficultyTimer) {
+    for (let i=0; i<monsterNumber; i++) {
+        let currentMonster = document.querySelector('.monster' + i);
+        let currentMonsterRow = currentMonster.dataset.row;
+        let currentMonsterCol = currentMonster.dataset.col;
+    }
 }
