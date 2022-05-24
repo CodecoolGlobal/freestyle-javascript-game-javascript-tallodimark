07bed867fabbf00980dd94daf4a0f7b3e7c0d083
Balint Meszaros <meszaros.balint65@gmail.com>
2022-05-24 14:45:55 +0200

    Add: spawn enemies with validation

--- game.js
+++ game.js
@@ -7,15 +7,33 @@ let monsterNumber = 0;
 let coinNumber = 0;
 let chestNumber = 0;
 let swordNumber = 0;
-let monstertHp = 0;
+let monsterHp = 0;
 let heroHp = 0;
 let difficultyTimer = 0;
 if (difficulty === 'Easy') {
     monsterNumber = 5;
+    coinNumber = 10;
+    chestNumber = 5;
+    swordNumber = 3;
+    monsterHp = 1;
+    heroHp = 10;
+    difficultyTimer = 3000;
 } else if (difficulty === 'Medium') {
     monsterNumber = 10;
+    coinNumber = 7;
+    chestNumber = 3;
+    swordNumber = 2;
+    monsterHp = 2;
+    heroHp = 7;
+    difficultyTimer = 2000;
 } else if (difficulty === 'Hard') {
     monsterNumber = 20;
+    coinNumber = 4;
+    chestNumber = 1;
+    swordNumber = 1;
+    monsterHp = 3;
+    heroHp = 3;
+    difficultyTimer = 1000;
 }
 
 initGame();
@@ -23,7 +41,7 @@ initGame();
 function initGame() {
     this.drawBoard();
     this.drawWalls()
-    this.populateBoard(monsterNumber);
+    this.populateBoard();
     this.placeHero();
     this.initKeyUp();
 }
@@ -38,7 +56,6 @@ function drawBoard () {
             this.addCell(rowElement, row, col);
             cellIndex++
         }
-        ;
     }
 
 }
@@ -55,6 +72,7 @@ function drawWalls () {
     let secondAndThirdCol = document.querySelectorAll('[data-col="' + secondCol + '"], [data-col="' + thirdCol + '"]')
     for (let element of secondAndThirdCol) {
         if (rowGates.includes(element.getAttribute('data-row'))) {
+            element.classList.add("gate")
             continue
         }
         element.classList.add('wall')
@@ -64,6 +82,7 @@ function drawWalls () {
     let secondAndThirdRow = document.querySelectorAll('[data-row="' + secondRow + '"], [data-row="' + thirdRow + '"]')
     for (let element of secondAndThirdRow) {
         if (colGates.includes(element.getAttribute('data-col'))) {
+            element.classList.add("gate")
             continue
         }
         element.classList.add('wall')
@@ -100,8 +119,57 @@ function initKeyUp () {
     })
 }
 
-function populateBoard(monsterNumber) {
-    console.log(monsterNumber);
+function checkNeighborCells(randomRow, randomCol) {
+    let rowBefore = 0;
+    if (randomRow != 0) {rowBefore = randomRow - 1}
+    let rowAfter = randomRow + 1;
+    if (rowAfter > rows - 1) {rowAfter = randomRow}
+    let colBefore = 0;
+    if (randomCol != 0) {colBefore = randomCol - 1}
+    let colAfter = randomCol + 1;
+    if (colAfter > cols - 1) {colAfter = randomCol}
+    for (let i = rowBefore; i < rowAfter + 1; i++) {
+        if (document.querySelector('[data-row="' + i + '"][data-col="' + randomCol + '"]')
+                .classList.contains("gate") ||
+            document.querySelector('[data-row="' + i + '"][data-col="' + randomCol + '"]')
+                .classList.contains("wall") ||
+            document.querySelector('[data-row="' + i + '"][data-col="' + randomCol + '"]')
+                .classList.contains("monster"))
+            return false
+    }
+    for (let j = colBefore; j < colAfter + 1; j++) {
+        if (document.querySelector('[data-row="' + randomRow + '"][data-col="' + j + '"]')
+                .classList.contains("gate") ||
+            document.querySelector('[data-row="' + randomRow + '"][data-col="' + j + '"]')
+                .classList.contains("wall") ||
+            document.querySelector('[data-row="' + randomRow + '"][data-col="' + j + '"]')
+                .classList.contains("monster"))
+            return false
+    }
+    return true
+}
+
+function populateBoard() {
+    for (let i = 0; i < monsterNumber; i++) {
+        let checkNeighbor = false
+        while (!checkNeighbor) {
+            let randomRow = getRandomInt(0, rows)
+            let randomCol = getRandomInt(0, cols)
+            checkNeighbor = checkNeighborCells(randomRow, randomCol)
+            if (checkNeighbor) {
+                let monster = document.querySelector('[data-row="' + randomRow + '"][data-col="' + randomCol + '"]')
+                monster.classList.add("monster")
+                monster.setAttribute("id", i)
+            }
+        document.querySelector(".stats").setAttribute("data-monster-" + i + "-hp", monsterHp)
+        }
+    }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
 function moveMonserts(monsterNumber, difficultyTimer) {
